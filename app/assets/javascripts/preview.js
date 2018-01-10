0abfc57cc64f2def2de6c6dfe6b1cc4ea7b9fd4d
Yudai Watanabe <lock0308@gmail.com>
2018-01-10 11:06:23 +0900

    サンプルコードをコピー

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -4,8 +4,24 @@
 //= require bootstrap
 //= require_tree .
 
-$(function() {
-  $("form").on("change", "input[type='file']", function() {
-    alert("Hallo");
-  });
+// $(function() {
+//   $("form").on("change", "input[type='file']", function() {
+//     var value = this.value;
+//     console.log(value);
+//   });
+// });
+
+$("form").on("change", "input[type='file']", function(){
+  if (this.files.length > 0) {
+    // 選択されたファイル情報を取得
+    var file = this.files[0];
+
+    // readerのresultプロパティに、データURLとしてエンコードされたファイルデータを格納
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = function() {
+      $('#main_image_uploader').attr('src', reader.result );
+    }
+  }
 });
