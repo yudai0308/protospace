b3c788e71153f32a0195413854eca1c47eec01e1
Yudai Watanabe <lock0308@gmail.com>
2018-01-11 11:19:47 +0900

    メインサムネイルのプレビューを表示

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,27 +1,25 @@
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap
-//= require_tree .
+// 参考サイト
+// http://hennayagyu.com/webhack/javascript/file-api%E3%81%AE%E7%94%BB%E5%83%8F%E3%83%97%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%E3%82%92%E3%81%A1%E3%82%83%E3%82%93%E3%81%A8%E7%90%86%E8%A7%A3%E3%81%99%E3%82%8B-2579
 
+$(function() {
+  $("form").on("change", "input[type='file']", function() {
+    var selectedFile = this.files[0];
+    // console.log(selectedFile);
+    var fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      var imageUri = event.target.result;
+      $("#main_image_uploader").html( '<img class="selected-img" src="' + imageUri + '">' );
+    };
+    fileReader.readAsDataURL(selectedFile);
+  });
+});
+
+// ✔︎ 画像サイズを親要素内に収める
+//   一度画像を選択したのち、別の画像もクリックできるようにする
+//   画像以外のファイルを選択できないようにして、選択されたらエラーメッセージを表示させる
+
+// ファイルが選択された時にアラートが出現
 // $(function() {
 //   $("form").on("change", "input[type='file']", function() {
-//     var value = this.value;
-//     console.log(value);
-//   });
-// });
-
-$("form").on("change", "input[type='file']", function(){
-  if (this.files.length > 0) {
-    // 選択されたファイル情報を取得
-    var file = this.files[0];
-
-    // readerのresultプロパティに、データURLとしてエンコードされたファイルデータを格納
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = function() {
-      $('#main_image_uploader').attr('src', reader.result );
-    }
-  }
-});
+//     alert("hello");
+// })});
