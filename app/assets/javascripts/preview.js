7466c3e14af10ee1825feec177e7a1a2a60de8b8
Yudai Watanabe <lock0308@gmail.com>
2018-01-12 18:01:49 +0900

    プレビュー画像の位置を中央にくるよう修正

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -13,48 +13,44 @@ $(function() {
   });
 });
 
-$(function() {
-  $("#prototype_captured_images_attributes_1_content").on("change", function() {
-    var selectedFile = this.files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(event) {
-      var imageUri = event.target.result;
-      $(".selected-img-1").replaceWith( '<img class="selected-img-1 img-preview" src="' + imageUri + '">' );
-    };
-    fileReader.readAsDataURL(selectedFile);
-  });
-});
+// $(function() {
+//   $("#prototype_captured_images_attributes_1_content").on("change", function() {
+//     var selectedFile = this.files[0];
+//     var fileReader = new FileReader();
+//     fileReader.onload = function(event) {
+//       var imageUri = event.target.result;
+//       $(".selected-img-1").replaceWith( '<img class="selected-img-1 img-preview" src="' + imageUri + '">' );
+//     };
+//     fileReader.readAsDataURL(selectedFile);
+//   });
+// });
 
-$(function() {
-  $("#prototype_captured_images_attributes_2_content").on("change", function() {
-    var selectedFile = this.files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(event) {
-      var imageUri = event.target.result;
-      $(".selected-img-2").replaceWith( '<img class="selected-img-2 img-preview" src="' + imageUri + '">' );
-    };
-    fileReader.readAsDataURL(selectedFile);
-  });
-});
+// $(function() {
+//   $("#prototype_captured_images_attributes_2_content").on("change", function() {
+//     var selectedFile = this.files[0];
+//     var fileReader = new FileReader();
+//     fileReader.onload = function(event) {
+//       var imageUri = event.target.result;
+//       $(".selected-img-2").replaceWith( '<img class="selected-img-2 img-preview" src="' + imageUri + '">' );
+//     };
+//     fileReader.readAsDataURL(selectedFile);
+//   });
+// });
 
-$(function() {
-  $("#prototype_captured_images_attributes_3_content").on("change", function() {
-    var selectedFile = this.files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(event) {
-      var imageUri = event.target.result;
-      $(".selected-img-3").replaceWith( '<img class="selected-img-3 img-preview" src="' + imageUri + '">' );
-    };
-    fileReader.readAsDataURL(selectedFile);
-  });
-});
+// $(function() {
+//   $("#prototype_captured_images_attributes_3_content").on("change", function() {
+//     var selectedFile = this.files[0];
+//     var fileReader = new FileReader();
+//     fileReader.onload = function(event) {
+//       var imageUri = event.target.result;
+//       $(".selected-img-3").replaceWith( '<img class="selected-img-3 img-preview" src="' + imageUri + '">' );
+//     };
+//     fileReader.readAsDataURL(selectedFile);
+//   });
+// });
 
 // ✔︎ 画像サイズを親要素内に収める
-//   一度画像を選択したのち、別の画像もクリックできるようにする
+// ✔︎ 一度画像を選択したのち、別の画像もクリックできるようにする
 //   画像以外のファイルを選択できないようにして、選択されたらエラーメッセージを表示させる
-
-// ファイルが選択された時にアラートが出現
-// $(function() {
-//   $("form").on("change", "input[type='file']", function() {
-//     alert("hello");
-// })});
+// サブサムネイルについてもプレビューを表示させる
+// 画像データに容量や大きさの制限をかける（エラーメッセージ）
