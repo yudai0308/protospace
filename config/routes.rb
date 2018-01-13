d369bb8a12f527226136e3edab343f694eac2591
根本大輔 <defmono112233@gmail.com>
2018-01-13 11:07:30 +0900

    [fix]コンフリクト対応として

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-  resources :prototypes, only: [:index, :new, :create, :show] do
+  resources :prototypes do
     resources :likes, only: [:create, :destroy]
   end
 
