7783ace98a6e88d7c5806f62f74214f56421521c
Yudai Watanabe <lock0308@gmail.com>
2018-01-16 10:38:33 +0900

    マージ用2

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -1,5 +1,3 @@
-// http://hennayagyu.com/webhack/javascript/file-api%E3%81%AE%E7%94%BB%E5%83%8F%E3%83%97%E3%83%AC%E3%83%93%E3%83%A5%E3%83%BC%E3%82%92%E3%81%A1%E3%82%83%E3%82%93%E3%81%A8%E7%90%86%E8%A7%A3%E3%81%99%E3%82%8B-2579
-
 $(function() {
   $("#prototype_captured_images_attributes_0_content").change(function() {
     var selectedFile = this.files[0];
@@ -18,19 +16,19 @@ $(function() {
   });
 });
 
-// $(function() {
-//   $("#prototype_captured_images_attributes_1_content").change(function() {
-//     var selectedFile = this.files[0];
-//     console.log(selectedFile);
-//     var fileReader = new FileReader();
-//     console.log(fileReader);
-//     fileReader.onload = function(event) {
-//       var imageUri = event.target.result;
-//       $(".selected-img-1").replaceWith( '<img class="selected-img-1 img-preview-sub" src="' + imageUri + '">' );
-//     };
-//     fileReader.readAsDataURL(selectedFile);
-//   });
-// });
+$(function() {
+  $("#prototype_captured_images_attributes_1_content").change(function() {
+    var selectedFile = this.files[0];
+    console.log(selectedFile);
+    var fileReader = new FileReader();
+    console.log(fileReader);
+    fileReader.onload = function(event) {
+      var imageUri = event.target.result;
+      $(".selected-img-1").replaceWith( '<img class="selected-img-1 img-preview-sub" src="' + imageUri + '">' );
+    };
+    fileReader.readAsDataURL(selectedFile);
+  });
+});
 
 // $(function() {
 //   $("#prototype_captured_images_attributes_2_content").change(function() {
@@ -55,9 +53,3 @@ $(function() {
 //     fileReader.readAsDataURL(selectedFile);
 //   });
 // });
-
-// ✔︎ 画像サイズを親要素内に収める
-// ✔︎ 一度画像を選択したのち、別の画像もクリックできるようにする
-//   画像以外のファイルを選択できないようにして、選択されたらエラーメッセージを表示させる
-// サブサムネイルについてもプレビューを表示させる
-// 画像データに容量や大きさの制限をかける（エラーメッセージ）
