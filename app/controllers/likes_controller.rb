49da0188ab10ed0363c9a985b03ee68fb03b5bd1
Yudai Watanabe <lock0308@gmail.com>
2018-01-17 19:08:39 +0900

    like機能の修正2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,12 @@
 class LikesController < ApplicationController
   def create
     like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    # @likes = Like.where(prototype_id: params[:prototype_id])
-    # @prototypes = Prototype.all
-    # @prototype = Prototype.find(params[:prototype_id])
     redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy if current_user.id == like.user_id
-    # @prototypes = Prototype.all
-    # @prototype = Prototype.find(params[:prototype_id])
-    # @likes = Like.where(prototype_id: params[:prototype_id])
     redirect_to controller: 'prototypes', action: 'show', id: params[:prototype_id]
   end
 end
