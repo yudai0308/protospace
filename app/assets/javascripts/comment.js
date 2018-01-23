78f6687d75832733b9618ac4bdf57b7d036962d9
Yudai Watanabe <lock0308@gmail.com>
2018-01-23 09:46:43 +0900

    コメント機能の非同期化

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,9 +1,11 @@
 $(function() {
   function buildHTML(comment) {
-    var html = `<li class="comment-list__content">${comment.content}</li>
-                <li class="comment-list__name">${comment.name}</li>
-                <li class="comment-list__date">${comment.date}</li>`
-  return html;
+    var html = `<ul class="comment-list">
+                  <li class="comment-list__content">${comment.content}</li>
+                  <li class="comment-list__name">${comment.name}</li>
+                  <li class="comment-list__date">${comment.date}</li>
+                </ul>`
+    return html;
   };
 
   $("#new_comment").on("submit", function(e) {
@@ -24,12 +26,13 @@ $(function() {
 
     .done(function(data) {
       var html = buildHTML(data);
-      $(".comment-list").prepend(html);
+      $(".comment-index").prepend(html);
       $(".input-comment").val("");
     })
 
     .fail(function() {
       alert("error");
     });
+    $(".comment-index").animate({scrollTop: 0}, 1000, "swing");
   });
 });
