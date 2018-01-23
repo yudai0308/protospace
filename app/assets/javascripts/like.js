6e3782a0c14d79c4b165968f0a5d1c0c6018f89b
Yudai Watanabe <lock0308@gmail.com>
2018-01-23 11:29:16 +0900

    likeの非同期化　途中

---
+++ app/assets/javascripts/like.js
@@ -0,0 +1,23 @@
+// $(function() {
+
+//     ver likeImage = "/assets/icon_red_heart-359c7031992df6009985e277af10a7c177411dbb5d041a98d18c69a302a8d2ae.png"
+//     ver unlikeImage = "/assets/icon_heart-1c7a40e09d343f6f88f0357fececc0d865470eecf51657a249b310bae85c88d0.png"
+
+//   $("#like-button").on("click", function(e) {
+//     e.preventDefault();
+//     var postPath = window.location.href "/likes"
+//     var deletePath = window.location.href "/likes/"
+
+//     if ( $(".unlike-img") ) {
+//       $.ajax({
+//         url: postPath,
+//         type: "POST",
+//         dataType: "json",
+//         processData: false,
+//         contentType: false
+//     })
+
+//     }
+
+//   })
+// })
