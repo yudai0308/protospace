1a114ef926121ae370164b230864d842c8cd29a0
城戸　真祐 <downmap555@gmail.com>
2018-01-24 09:14:26 +0900

    Tag show index

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,29 @@
 class TagsController < ApplicationController
+  def index
+    @tags = []
+    Tag.order("name ASC").each do |tag|
+      i = 0
+      @tags.each do |t|
+        if tag.name != t.name
+          i += 1
+        end
+      end
+      if i == @tags.length
+        @tags << tag
+      end
+    end
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    @prototypes = []
+    Prototype.all.each do |prototype|
+      prototype.tags.each do |tag|
+        if tag.name == @tag.name
+          @prototypes << prototype
+          break
+        end
+      end
+    end
+  end
 end
